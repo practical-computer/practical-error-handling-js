5122c5db371ee83ac887c81454a8eeaa36b98481
Thomas Cannon <tcannon00@gmail.com>
2025-04-18 18:25:26 -0400

    Add `docs` script; `jsdoc` configuration

---
+++ jsdoc.config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+    plugins: ['plugins/markdown'],
+    "source": {
+      "include": [
+          "index.js",
+          "error-handling-element.js",
+          "src",
+          "README.md"
+      ],
+    }
+};
