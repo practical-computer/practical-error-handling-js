f7ca5faf24506bdc2bc6188beac1f1f1b37e5f34
Thomas Cannon <tcannon00@gmail.com>
2025-04-18 18:19:21 -0400

    WIP: Experimenting with JSDoc

--- src/element-utilities.js
+++ src/element-utilities.js
@@ -1,3 +1,8 @@
+/**
+ * Returns the `validationMessage` for an element if it is present
+ * @param {Element} element
+ * @return {string} The `element.validationMessage`, or an empty string
+ */
 export function generateValidationMessage(element) {
   if(element.validity.valid){ return ''}
   return element.validationMessage
