5dc4becedd2292ee9c1e70ea49280c537841d24b
Thomas Cannon <tcannon00@gmail.com>
2025-04-23 16:04:21 -0400

    Rename `applyErrorMappingToFormFromUnprocessableEntityResponseResponse`
    * Renamed to `applyErrorMappingFromResponse`; the previous method name
    	was simply too long
    
    Co-authored-by: Konnor Rogers <konnor5456@gmail.com>

--- src/plugins/mrujs.js
+++ src/plugins/mrujs.js
@@ -1,12 +1,12 @@
-import {applyErrorMappingToFormFromUnprocessableEntityResponseResponse} from '../request-processing.js'
+import {applyErrorMappingFromResponse} from '../request-processing.js'
 
 /**
- * Calls {@link applyErrorMappingToFormFromUnprocessableEntityResponseResponse}, passing along the
+ * Calls {@link applyErrorMappingFromResponse}, passing along the
  * `element` and `response` from the given `event`
  * @param {Event} event - The event that was fired for a fetch response, ideally `ajax:response:error`
  * @param {FormElement} event.detail.element - The `form` event that triggered the event
  * @param {Response} event.detail.response - The `Response` from the fetch request
  */
 export async function unprocessableEntityResponseHandler(event) {
-  await applyErrorMappingToFormFromUnprocessableEntityResponseResponse(event.detail.element, event.detail.response)
+  await applyErrorMappingFromResponse(event.detail.element, event.detail.response)
 }
